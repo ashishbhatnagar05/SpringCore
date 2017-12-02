0f95790ada73bd2ce0165ec059be254074827e8b
kaal-PC\kaal <ashishbhatnagar05@gmail.com>
2017-12-02 21:18:44 +0530

    property DI, Section: 2 lecture: 9

--- src/main/java/com/spring/io/Person.java
+++ src/main/java/com/spring/io/Person.java
@@ -3,6 +3,16 @@ package com.spring.io;
 public class Person {
 	private int id;
 	private String name;
+	private int textId;
+
+	public int getTextId() {
+		return textId;
+	}
+
+	//setter name is important.Bean should use the same convention.
+	public void setTextId(int textId) {
+		this.textId = textId;
+	}
 
 	public Person(int id, String name) {
 		super();
@@ -15,9 +25,11 @@ public class Person {
 		// TODO Auto-generated constructor stub
 	}
 
+	
+
 	@Override
 	public String toString() {
-		return "Person [id=" + id + ", name=" + name + "]";
+		return "Person [id=" + id + ", name=" + name + ", textId=" + textId + "]";
 	}
 
 	public void speak() {

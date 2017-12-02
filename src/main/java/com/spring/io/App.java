491ecdbcaea822c4894e2ea0c685217ed7cbfae2
kaal-PC\kaal <ashishbhatnagar05@gmail.com>
2017-12-03 04:37:06 +0530

    initial structure of jdbc template using postgres, Section: 6 Lecutre: 43

--- src/main/java/com/spring/io/App.java
+++ src/main/java/com/spring/io/App.java
@@ -1,13 +1,18 @@
 package com.spring.io;
 
+import java.util.List;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class App {
 	public static void main(String[] args) {
 		ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
-		Robot robot = context.getBean("robot", Robot.class);
-		System.out.println(robot);
+		OffersDAO offersDAO = context.getBean("offersDAO", OffersDAO.class);
+		List<Offers> offers = offersDAO.getOffers();
+		for (Offers o : offers) {
+			System.out.println(o);
+		}
 		((ClassPathXmlApplicationContext) context).close();
 	}
 }
---
+++ src/main/java/com/spring/io/OffersDAO.java
@@ -0,0 +1,37 @@
+package com.spring.io;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Component;
+
+@Component("offersDAO")
+public class OffersDAO {
+
+	private JdbcTemplate jdbc;
+
+	@Autowired
+	public void setDataSource(DataSource jdbc) {
+		this.jdbc = new JdbcTemplate(jdbc);
+	}
+
+	public List<Offers> getOffers() {
+		return jdbc.query("select * from mydb.offers", new RowMapper<Offers>() {
+
+			public Offers mapRow(ResultSet rs, int rowNum) throws SQLException {
+				Offers offer = new Offers();
+				offer.setId(rs.getInt("id"));
+				offer.setName(rs.getString("name"));
+				offer.setEmail(rs.getString("email"));
+				offer.setText(rs.getString("text"));
+				return offer;
+			}
+		});
+	}
+}

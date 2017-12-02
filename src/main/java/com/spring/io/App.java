26d6e023070d488df39ad1a50ecc47b6efd3a9c9
kaal-PC\kaal <ashishbhatnagar05@gmail.com>
2017-12-02 21:02:38 +0530

    changed context, Section1 lecture 7

--- src/main/java/com/spring/io/App.java
+++ src/main/java/com/spring/io/App.java
@@ -1,13 +1,17 @@
 package com.spring.io;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 public class App {
 	public static void main(String[] args) {
-		ApplicationContext context = new FileSystemXmlApplicationContext("beans.xml");
-
+		ApplicationContext context =
+				// new FileSystemXmlApplicationContext("/src/main/resources/beans.xml");
+				new ClassPathXmlApplicationContext("beans.xml");
 		Person person = context.getBean("person", Person.class);
 		person.speak();
+		((ClassPathXmlApplicationContext) context).close();// to make sure context got destroyed. ApplicationContext
+															// doesn't have close method, so we did casting
 	}
 }

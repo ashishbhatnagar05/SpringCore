45e42a5797c3daf39e651972d5c5f83d6b730852
kaal-PC\kaal <ashishbhatnagar05@gmail.com>
2017-12-03 13:49:54 +0530

    handled dataaccessexception, Section: 6 Lecutre: 45

--- src/main/java/com/spring/io/App.java
+++ src/main/java/com/spring/io/App.java
@@ -4,14 +4,21 @@ import java.util.List;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.dao.DataAccessException;
 
 public class App {
 	public static void main(String[] args) {
 		ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
 		OffersDAO offersDAO = context.getBean("offersDAO", OffersDAO.class);
-		List<Offers> offers = offersDAO.getOffers();
-		for (Offers o : offers) {
-			System.out.println(o);
+		try {
+			List<Offers> offers = offersDAO.getOffers();
+			for (Offers o : offers) {
+				System.out.println(o);
+			}
+		} catch (DataAccessException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
 		((ClassPathXmlApplicationContext) context).close();
 	}
